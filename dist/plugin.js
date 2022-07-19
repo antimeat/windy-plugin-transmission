ef7d28034dcd6e18edb9deeff90aa5cee14b0252
antimeat <anti.meat@gmail.com>
2022-07-20 06:43:54 +0800

    version update

--- dist/plugin.js
+++ dist/plugin.js
@@ -8,7 +8,7 @@ W.loadPlugin(
 /* Mounting options */
 {
   "name": "windy-plugin-transmission",
-  "version": "1.1.0",
+  "version": "1.1.1",
   "author": "Darryl Vink",
   "repository": {
     "type": "git",
